71ffd4f3a8bbdc2bb5499297b76e39f8b9f0a11c
Olly <124927810+OllyOlli@users.noreply.github.com>
2024-03-20 20:08:34 +0100

    Delete hw15exceptions directory
    -

--- hw15exceptions/ExchangeRecord.java
+++
@@ -1,7 +0,0 @@
-package cz.robodreams.javadeveloper.homeworks.hw15exceptions;
-//
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-public record ExchangeRecord(String currency, BigDecimal pricePerCurrency, LocalDate day)  {
-}
