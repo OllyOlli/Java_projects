f11e191603f75494376e65cbec45acdafb3b7aa5
Olly <124927810+OllyOlli@users.noreply.github.com>
2024-05-07 20:05:58 +0200

    Update PassengerPlaneFlight.java

--- hw12objectinterfaces/PassengerPlaneFlight.java
+++ hw12objectinterfaces/PassengerPlaneFlight.java
@@ -1,4 +1,4 @@
-package cz.robodreams.javadeveloper.homeworks.hw12objectinterfaces;
+package hw12objectinterfaces;
 
 public class CargoPlaneFlight extends AbstractAircraftFlight {
     private double maxCargoWeight;
