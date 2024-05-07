5890c035b29a6947fc5800789c8f7bd6eb3e623b
Olly <124927810+OllyOlli@users.noreply.github.com>
2024-05-07 20:06:22 +0200

    Update CargoPlaneFlight.java

--- hw12objectinterfaces/CargoPlaneFlight.java
+++ hw12objectinterfaces/CargoPlaneFlight.java
@@ -1,4 +1,4 @@
-package cz.robodreams.javadeveloper.homeworks.hw12objectinterfaces;
+package hw12objectinterfaces;
 
 public class CargoPlaneFlight extends AbstractAircraftFlight {
     // Atributy pro maximální váhu nákladu a typ nákladu
