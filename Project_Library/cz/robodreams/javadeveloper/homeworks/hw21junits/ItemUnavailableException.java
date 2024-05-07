b9c19e190e9cc354010e3852a49e9840e997f6ac
Olly <124927810+OllyOlli@users.noreply.github.com>
2024-05-07 20:31:07 +0200

    Update ItemUnavailableException.java

--- Project_Library/cz/robodreams/javadeveloper/homeworks/hw21junits/ItemUnavailableException.java
+++ Project_Library/cz/robodreams/javadeveloper/homeworks/hw21junits/ItemUnavailableException.java
@@ -1,4 +1,4 @@
-package cz.robodreams.javadeveloper.homeworks.hw21junits;
+package hw21junits;
 
 // Vlastní výjimka pro situaci, kdy je položka nedostupná
 public class ItemUnavailableException extends Exception {
