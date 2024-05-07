6190e2dd2a509a053c1b19d997610723381d51a2
Olly <124927810+OllyOlli@users.noreply.github.com>
2024-05-07 20:27:40 +0200

    Update UserNotFoundException.java

--- Project_Library/cz/robodreams/javadeveloper/homeworks/hw21junits/UserNotFoundException.java
+++ Project_Library/cz/robodreams/javadeveloper/homeworks/hw21junits/UserNotFoundException.java
@@ -1,4 +1,4 @@
-package cz.robodreams.javadeveloper.homeworks.hw21junits;
+package hw21junits;
 
 // Třída reprezentující výjimku, kdy uživatel nebyl nalezen
 public class UserNotFoundException extends Exception {
